90013a84a2903432b6e18681d0751e21ed478701
BrockH <brock.horton@gmail.com>
2020-05-27 18:39:55 -0400

    push to the database

--- proj-2/Unsolved/routes/htmlRoutes.js
+++ proj-2/Unsolved/routes/htmlRoutes.js
@@ -1,44 +1,35 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Load index page
-  // app.get("/", function(req, res) {
-  //   db.Example.findAll({}).then(function(dbExamples) {
-  //     res.render("index", {
-  //       msg: "Welcome!",
-  //       examples: dbExamples
-  //     });
-  //   });
-  // });
-
   app.get("/", function(req, res) {
-    db.Drinks.findAll({}).then(function(dbDrinks) {
+    db.drinks.findAll({}).then(function(dbDrinks) {
       res.render("index", {
         msg: "Welcome!",
         drinks: dbDrinks
       });
     });
   });
-
   app.get("/register", function(req, res) {
-    db.Drinks.findAll({}).then(function(dbDrinks) {
+    db.drinks.findAll({}).then(function(dbDrinks) {
       res.render("signup", {
         msg: "Welcome!",
         drinks: dbDrinks
       });
     });
   });
-
-
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  app.get("/api/newDrink", function() {
+    db.drinks.findAll({}).then(function() {});
   });
-
+  // Load example page and pass in an example by id
+  app.get("/api/drink/:Name", function(req, res) {
+    db.drinks
+      .findOne({ where: { Name: req.params.Name } })
+      .then(function(dbExample) {
+        res.render("example", {
+          example: dbExample
+        });
+      });
+  });
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
